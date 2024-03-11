7b18e256a1ef762bd803d1d0c1b001320cc05f53
Ben Cavins <ben.cavins@flatironschool.com>
2024-03-11 14:44:10 -0400

    do lab

--- lib/parrot.py
+++ lib/parrot.py
@@ -4,3 +4,17 @@ def parrot(my_string="Squawk!"):
     print(my_string)
     return my_string
 
+
+# example of a useful default parameter
+def sort(my_list, reverse=False):
+    if reverse:
+        # sort descending
+        pass
+    else:
+        # sorts ascending
+        pass
+
+# don't need to pass anything if we want to sort ascending
+sort([])
+# we only need to pass True if we want to sort descending
+sort([], True)
